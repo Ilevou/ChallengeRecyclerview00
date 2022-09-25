c141e46b9c551767c96ec303e3da0422da67a87b
Ilektra Vouroudi <Ilektra.vouroudi@sap.com>
2022-09-25 15:28:14 +0200

    adapter of the recycler view with a custom theme

--- app/src/main/java/com/ilektra/challengemyrecyclerview/MainActivity.kt
+++ app/src/main/java/com/ilektra/challengemyrecyclerview/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.ilektra.challengemyrecyclerview
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ilektra.challengemyrecyclerview.R
 
 class MainActivity : AppCompatActivity(){
     private lateinit var editText: EditText
     private lateinit var button: Button
+    private lateinit var adapter: MyAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/ilektra/challengemyrecyclerview/MyAdapter.kt
@@ -0,0 +1,39 @@
+package com.ilektra.challengemyrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MyAdapter(private val data: ArrayList<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    inner class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(property: String, index: Int) {
+            val title = view.findViewById<TextView>(R.id.textView)
+
+
+            title.text = property
+
+        }
+
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view1 =
+            LayoutInflater.from(parent.context).inflate(R.layout.card_view_design, parent, false)
+        return MyViewHolder(view1)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(data[position], position)
+    }
+
+
+}
